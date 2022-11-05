60ddf01f8d472c2e91e12fb09e89e29905519d26
Nicholas Sielicki <git@opensource.nslick.com>
2022-11-05 18:40:14 -0500

    nix: bump to 22.05, update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A minimal status bar for macOS";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/21.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/22.05;
   inputs.flake-utils.url = github:numtide/flake-utils;
 
   outputs = { self, nixpkgs, flake-utils }:
